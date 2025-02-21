7706c87da32d99e4c5ec8a292c2b3097d743e8df
estoneman <elstoneman11@gmail.com>
2025-02-21 08:53:42 -0600

    add ability to read from batch file

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,7 @@
 -- src/main.lua
 
+local capacity = 90
+
 ---capture command output into stdout
 ---@param cmd string
 ---@param raw boolean
@@ -19,23 +21,16 @@ end
 
 ---check if file exists
 ---@param file string
+---@return boolean
 local function exists(file)
-    local ok, err, code = os.rename(file, file)
-    if not ok then
-        if code == 13 then
-            -- permissiond denied, but still exists
-            return true
-        end
+    local handle, _ = io.open(file, 'r')
+    if handle == nil then
+        return false
     end
 
-    return ok, err
-end
+    handle:close()
 
----check if `path` is a directory
----@param path string
----@return boolean
-local function isdir(path)
-    return exists(path .. '/')
+    return true
 end
 
 ---invoke `yt-dlp` to convert YouTube video to a WAV file
@@ -43,12 +38,12 @@ end
 ---@param dest string
 ---@param sleep integer
 local function yt_dlp(track, dest, sleep)
-    if not isdir(dest) then
+    if not exists(dest) then
         os.execute('mkdir ' .. dest)
     end
 
     local date = os.date("%Y%m%d")
-    if not isdir('archive') then
+    if not exists('archive') then
         os.execute('mkdir archive')
     end
     local archive = './archive/' .. date
@@ -67,34 +62,88 @@ local function yt_dlp(track, dest, sleep)
             "ytsearch:%s"
     ]], sleep, dest, archive, track)
 
-    print(cmd)
+    print('converting ' .. track)
+
     os.execute(cmd)
 end
 
----return length of table
----@param t table
-local function tableLen(t)
-    local count = 0
-    for _ in pairs(t) do count = count + 1 end
-    return count
+---read lines from file and return each line as its own in element in a table
+---@param file string
+---@return table, integer, string
+local function readLines(file)
+    if not exists(file) then
+        return {}, 0, string.format('%s: No such file or directory', file)
+    end
+
+    local handle, _ = io.open(file, 'r')
+    if handle == nil then
+        return {}, 0, string.format('fatal: could not open file with read permissions')
+    end
+
+    ---@type string
+    local raw = handle:read('*a')
+
+    ---@type table
+    local t = {}
+    local cnt = 0
+    for line in raw:gmatch('[^\n]+') do
+        table.insert(t, line)
+        cnt = cnt + 1
+    end
+
+    return t, cnt, ""
+end
+
+---check if directory is taking up too much space on its mountpoint
+---@param dir string
+---@return boolean
+local function atCapacity(dir)
+    local proc, _ = io.popen('df ' .. dir .. '| awk \'{ print $5 }\' | tail -1')
+    if proc == nil then
+        return false
+    end
+
+    local diskUsage
+    local out = proc:read('a')
+    ---@type number
+    for v in string.gmatch(out, '%d+') do
+        diskUsage = tonumber(v)
+        if diskUsage == nil then
+            return false
+        end
+    end
+
+    local ret = false
+    if diskUsage >= capacity then
+        ret = true
+    end
+
+    proc:close()
+
+    return ret
 end
 
 local function main()
-    local tracks = {
-        'let the bass kick, lemtom',
-        'u won\'t see me, rossi.',
-        'how u make me feel, dj seinfeld',
-        'out the fire (at the hotel) [feat. eunice collins], franky rizardo & ros t',
-        'spanish fly, 2000 and one',
-        'good 4 u, luuk van fijk & kolter',
-        'baby be mine, demarzo',
-        'make it, demarzo',
-    }
+    local tracks, count, msg = readLines('./batch/20250221')
+    if string.len(msg) > 0 then
+        print(msg)
+        os.exit(1)
+    end
 
     local dest = os.getenv('HOME') .. '/.local/share/rekordbox/usb/tracks/wav'
-    local nTracks = tableLen(tracks)
+    if not exists(dest) then
+        print('error: ' .. dest .. ' does not exist.. is it mounted?')
+        os.exit(1)
+    end
+
+    if atCapacity(dest) then
+        print('error: ' .. dest .. ' is at >=' .. capacity .. '% capacity.. get a new usb')
+        os.exit(1)
+    end
+
+    local sleep = math.floor(1.05 ^ count)
     for _, track in pairs(tracks) do
-        yt_dlp(track, dest, math.floor(1.05 ^ nTracks))
+        yt_dlp(track, dest, sleep)
     end
 end
 
