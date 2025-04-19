59a4d34cede962ddc731aed24941a8e88290021f
Ethan Stoneman <elstoneman11@gmail.com>
2025-04-19 09:55:01 -0500

    add check for batch directory existence

--- src/batch.lua
+++ src/batch.lua
@@ -1,10 +1,14 @@
 -- src/batch.lua
+local util = require("util")
 
 local batch_path = "batch/" .. os.date("%Y%m%d")
 
 ---enter vim editor to create a new batch
 local function new()
-    os.execute("/usr/local/bin/nvim " .. batch_path)
+    if not util.pathExists('batch') then
+        os.execute('mkdir -p batch')
+    end
+    os.execute("nvim " .. batch_path)
 end
 
 new()
